065b0d958f0de25ab5eef11c28c87710dfeac7d7
audrey.huguenin <audrey.huguenin@heig-vd.ch>
2020-03-14 17:10:27 +0100

    added member-details component + beginning content

--- packages/frontend/src/app/app-routing.module.ts
+++ packages/frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { MemberDetailsComponent } from './pages/member-details/member-details.component'
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'member', component: MemberDetailsComponent },
   { path: '**', component: NotFoundComponent }
 ];
 
--- packages/frontend/src/app/pages/pages.module.ts
+++ packages/frontend/src/app/pages/pages.module.ts
@@ -4,9 +4,10 @@ import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { UserModule } from '../user/user.module';
 import { AppMaterialModule } from '../app-material.module';
+import { MemberDetailsComponent } from './member-details/member-details.component';
 
 @NgModule({
-  declarations: [HomeComponent, NotFoundComponent],
+  declarations: [HomeComponent, NotFoundComponent, MemberDetailsComponent],
   imports: [CommonModule, UserModule, AppMaterialModule]
 })
 export class PagesModule {}
