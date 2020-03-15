8741f3b74907fd17b38f2b10568b98ecd6318e7d
Arthur Verdon <verdon.arthur@gmail.com>
2020-03-15 15:03:15 +0100

    fix problem during merge

--- packages/frontend/src/app/app-material.module.ts
+++ packages/frontend/src/app/app-material.module.ts
@@ -8,6 +8,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatTableModule } from '@angular/material/table';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   imports: [
@@ -19,7 +20,8 @@ import { MatTableModule } from '@angular/material/table';
     MatFormFieldModule,
     MatInputModule,
     MatListModule,
-    MatTableModule
+    MatTableModule,
+    MatGridListModule
   ],
   exports: [
     MatToolbarModule,
@@ -30,7 +32,8 @@ import { MatTableModule } from '@angular/material/table';
     MatFormFieldModule,
     MatInputModule,
     MatListModule,
-    MatTableModule
+    MatTableModule,
+    MatGridListModule
   ]
 })
 export class AppMaterialModule {}
