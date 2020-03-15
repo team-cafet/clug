9e4422a4807660f854b3074d8fc13ff12e27948b
Arthur Verdon <verdon.arthur@gmail.com>
2020-03-15 13:10:16 +0100

    remove useless code

--- packages/frontend/src/app/member/member.module.ts
+++ packages/frontend/src/app/member/member.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MemberLineComponent } from './member-line/member-line.component';
 
 
 
 @NgModule({
-  declarations: [MemberLineComponent],
+  declarations: [],
   imports: [
     CommonModule
   ]
