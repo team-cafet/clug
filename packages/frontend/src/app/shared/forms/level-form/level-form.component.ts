1b996b2137f44e418549efce8461d68f77cea8b8
Sébastien Aubert <sebastien.aubert@hotmail.ch>
2020-06-06 12:52:38 +0200

    club and level forms

--- packages/frontend/src/app/shared/forms/level-form/level-form.component.ts
+++ packages/frontend/src/app/shared/forms/level-form/level-form.component.ts
@@ -5,59 +5,31 @@ import { cleanObjectForSending } from 'src/app/core/functions';
 
 @Component({
   selector: 'app-level-form',
-  template: `
-  <div>
-    <form>
-      <mat-card class="club-card">
-        <mat-form-field class="full-width">
-          <mat-label>Name</mat-label>
-          <input
-            matInput
-            [ngModelOptions]="{ standalone: true }"
-            [(ngModel)]="level.name"
-          />
-        </mat-form-field>
-        <mat-form-field class="full-width">
-          <mat-label>Description</mat-label>
-          <textarea
-            matInput
-            [ngModelOptions]="{ standalone: true }"
-            [(ngModel)]="level.description"
-          ></textarea>
-        </mat-form-field>
-        <mat-card-actions>
-          <button mat-button type="button" color="primary" (click)="save()">
-            Save
-          </button>
-        </mat-card-actions>
-      </mat-card>
-    </form>
-  </div>`,
+  templateUrl: './level-form.component.html',
   styleUrls: [ './level-form.component.scss' ]
 })
 export class LevelFormComponent {
   @Input() level: Level;
   @Output() saved = new EventEmitter<Level>();
 
-  constructor(
-    private readonly levelSrv: LevelService
-  ) { }
+  constructor(private readonly levelSrv: LevelService) {}
 
   async save() {
     const backupLevel = this.level;
     try {
       if (this.level.id) {
-        this.level = await this.levelSrv.saveOne(cleanObjectForSending(this.level));
+        this.level = await this.levelSrv.saveOne(
+          cleanObjectForSending(this.level)
+        );
       } else {
-        this.level = await this.levelSrv.addOne(cleanObjectForSending(this.level));
+        this.level = await this.levelSrv.addOne(
+          cleanObjectForSending(this.level)
+        );
       }
-
       this.saved.emit(this.level);
-
     } catch (error) {
       console.error(error);
       this.level = backupLevel;
     }
   }
-
 }
