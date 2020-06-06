1c3f1a7b503dac8a61eb1f321e9993bade17fcb2
Sébastien Aubert <sebastien.aubert@hotmail.ch>
2020-06-06 13:18:23 +0200

    generic components

--- packages/frontend/src/app/shared/generic/rest-view/rest-view.component.ts
+++ packages/frontend/src/app/shared/generic/rest-view/rest-view.component.ts
@@ -4,26 +4,7 @@ import { RESTService } from 'src/app/core/services/rest.service';
 
 @Component({
   selector: 'app-rest-view',
-  template: `
-  <div>
-    <div>
-      <button
-        mat-mini-fab
-        color="primary"
-        color="primary"
-        [routerLink]="['/', 'app', resourceName, 'add']"
-      >
-        <mat-icon>add</mat-icon>
-      </button>
-    </div>
-    <div>
-      <app-rest-data-table
-        [resourceColumns]="resourceColumns"
-        [resourceName]="resourceName"
-        [resourceAPI]="resourceName"
-        action='true'></app-rest-data-table>
-    </div>
-  </div>`,
+  template: ``,
   styleUrls: [ './rest-view.component.scss' ]
 })
 export class RestViewComponent implements OnInit {
