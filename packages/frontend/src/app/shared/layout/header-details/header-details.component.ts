1bbf2554f947f821bfb8e0608f4527402cfccdcf
Sébastien Aubert <sebastien.aubert@hotmail.ch>
2020-06-06 13:20:58 +0200

    layout/header-details component

--- packages/frontend/src/app/shared/layout/header-details/header-details.component.ts
+++ packages/frontend/src/app/shared/layout/header-details/header-details.component.ts
@@ -2,16 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header-details',
-  template: `
-  <mat-toolbar color="primary">
-    <mat-toolbar-row>
-      <button mat-icon-button type="button" [routerLink]="['/', 'app', resourceName]">
-        <mat-icon>arrow_back</mat-icon>
-      </button>
-      <span>{{resourceName}}</span>
-    </mat-toolbar-row>
-  </mat-toolbar>
-  `,
+  templateUrl: './header-details.component.html',
   styleUrls: [ './header-details.component.scss' ]
 })
 export class HeaderDetailsComponent implements OnInit {
