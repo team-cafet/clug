7a4973d6fc1927ca60b134879ada87b9f401a44c
Sébastien Aubert <sebastien.aubert@hotmail.ch>
2020-08-12 16:17:35 +0200

    request to retrieve all plans when component is loaded

--- packages/frontend-v2/src/components/pages/MembershipPlan.tsx
+++ packages/frontend-v2/src/components/pages/MembershipPlan.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { IMembershipPlan } from '../../libs/interfaces/membershipPlan.interface';
+import { membershipPlanService } from '../../services/membershipPlan.service';
 
 export const MembershipPlan = () => {
+  const [plans, setPlans] = useState<IMembershipPlan[]>([])
+  let listOfPlans = [];
+
+  const getAllPlans = async (): Promise<void> =>{
+    const plans = await membershipPlanService.getAll();
+    setPlans(plans?.data);
+  }
+  getAllPlans()
   return (
     <>
       <h1>Gestion des abonnements</h1>
+      <div className="row">
+          <table className="table">
+            <tbody>
+{
+plans.map((plan) =><MembershipPlanRow plan={plan} key={plan.id} />)
+}
+</tbody>
+              </table>
+              </div>
     </>
+  );
+};
+
+const MembershipPlanRow = (props: { plan: IMembershipPlan }) => {
+  const { plan } = props;
+
+  return (
+    <tr>
+      <td>{plan.id}</td>
+      <td>{plan.price}</td>
+    </tr>
   );
 };
