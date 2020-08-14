86b1f3d6371b086fd4bd285e0c9f9d0c51af96d1
Sébastien Aubert <sebastien.aubert@hotmail.ch>
2020-08-14 17:18:58 +0200

    re-organize sidebar in separate component

--- packages/frontend-v2/src/components/template/AdminLayout.tsx
+++ packages/frontend-v2/src/components/template/AdminLayout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './AdminLayout.scss';
+import { Sidebar } from './Sidebar';
 
 interface IProps {
   children: any;
@@ -30,40 +31,10 @@ export const AdminLayout = (props: IProps) => {
   return (
     <>
       <div id="adminlayout">
-        <div className="navbar navbar-dark bg-primary flex-md-nowrap p-0">
-          <Link
-            to="/admin/dashboard"
-            className="navbar-brand col-md-3 col-lg-2 mr-0 px-3"
-          >
-            Clug
-          </Link>
-
-          <ul className="navbar-nav px-3">
-            <li className="nav-item text-nowrap">
-              <Link className="nav-link" to="/logout">
-                Sign out
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-3 col-lg-2 d-md-block sidebar collapse">
-              <ul id="sidebarMenu" className="nav flex-column">
-                {adminLinks.map((link) => (
-                  <li className="nav-item" key={link.to}>
-                    <Link to={link.to} className="nav-link">
-                      {link.displayName}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className="col-md-9 ml-sm-auto col-lg-10 main">
+        <Sidebar links= {adminLinks}/>
+        <div className="col-md-9 ml-sm-auto col-lg-10 main">
               {props.children}
             </div>
-          </div>
-        </div>
       </div>
     </>
   );
