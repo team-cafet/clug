9c5284bc7c17e2c6016b93d4d333fc27ba4487b5
Sébastien Aubert <sebastien.aubert@hotmail.ch>
2020-09-24 09:41:18 +0200

    Use transaction

--- packages/backend-v2/src/controllers/payment.ts
+++ packages/backend-v2/src/controllers/payment.ts
@@ -1,6 +1,6 @@
 import { RESTController } from '../libs/classes/RESTController';
 import { Payment } from '../models/Payment';
-import { getRepository } from 'typeorm';
+import { getConnection, getRepository } from 'typeorm';
 import { Request, Response } from 'express';
 import { PaymentRequest } from '../models/PaymentRequest';
 
@@ -15,28 +15,44 @@ export class PaymentCtrl extends RESTController<Payment> {
   ): Promise<Response> => {
     const { payment, memberShip } = req.body;
     if (!payment || !memberShip) return res.status(400).send('wrong request');
+    let newPayment;
     try {
-      const requestEntity = getRepository(PaymentRequest).create([
-        {
-          amount: memberShip.plan.price,
-          date: new Date(),
-          description: 'demande crée automatiquement'
+      const transacResult = await getConnection().transaction(
+        async (transactionalEntityManager) => {
+          const requestEntity = transactionalEntityManager
+            .getRepository(PaymentRequest)
+            .create([
+              {
+                amount: memberShip.plan.price,
+                date: new Date(),
+                description: 'demande crée automatiquement'
+              }
+            ]);
+          const newRequest = (
+            await transactionalEntityManager
+              .getRepository(PaymentRequest)
+              .save(requestEntity)
+          )[0];
+          if (!newRequest) throw new Error();
+          const paymentEntity = transactionalEntityManager
+            .getRepository(Payment)
+            .create([
+              {
+                amount: newRequest.amount,
+                date: newRequest.date,
+                member: memberShip.member,
+                paymentRequest: { id: newRequest.id }
+              }
+            ]);
+            newPayment = (
+            await transactionalEntityManager
+              .getRepository(Payment)
+              .save(paymentEntity)
+          )[0];
+          if (!newPayment) throw new Error();
         }
-      ]);
-      const newRequest = (await getRepository(PaymentRequest).save(
-        requestEntity
-      ))[0];
-      if(!newRequest) throw new Error();
-      const paymentEntity = getRepository(Payment).create([
-        {
-          amount: newRequest.amount,
-          date: newRequest.date,
-          member: memberShip.member,
-          paymentRequest: {id: newRequest.id}
-        }
-      ]);
-      const newPayment = (await getRepository(Payment).save(paymentEntity))[0];
-      if(!newPayment) throw new Error();
+      );
+
       return res.status(201).send(newPayment);
     } catch (error) {
       return res.status(400).send('Error during payment creation');
