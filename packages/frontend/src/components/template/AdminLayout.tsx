0624181eff96c0d0cc734954a76aaebdcda4b3c1
audrey.huguenin <audrey.huguenin@heig-vd.ch>
2020-10-15 17:10:21 +0200

    merge conflicts

--- packages/frontend/src/components/template/AdminLayout.tsx
+++ packages/frontend/src/components/template/AdminLayout.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-//import { Button, Nav } from 'react-bootstrap';
+import { Button, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
 
