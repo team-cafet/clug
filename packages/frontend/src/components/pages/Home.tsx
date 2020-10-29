aee6fb0e06a1d5f1a05b906758baa122a5d263ce
Sébastien Aubert <43539488+sebibigbob8@users.noreply.github.com>
2020-10-29 17:07:58 +0100

    Feature/routing (#61)
    * fix logout and home routing
    
    * Use bootstrap class

--- packages/frontend/src/components/pages/Home.tsx
+++ packages/frontend/src/components/pages/Home.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 export const Home = () => {
+  const history = useHistory();
+  history.push('/login');
+  //Until this page is designed to welcome prospect or client, redirect straight to login page.
   return (
     <>
       <h1>Bienvenue sur Clug App</h1>
