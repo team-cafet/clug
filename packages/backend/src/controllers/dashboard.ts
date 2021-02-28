f43e8fe5c02334bd34abfa0d303df661238eca4b
Sebastien Aubert <sebastien.aubert@hotmail.ch>
2021-02-28 13:39:55 +0100

    Return total members sum

--- packages/backend/src/controllers/dashboard.ts
+++ packages/backend/src/controllers/dashboard.ts
@@ -2,10 +2,12 @@ import { Response, Request } from 'express';
 import { Member } from '../models/Member';
 import { User } from '../models/User';
 import { getRepository } from 'typeorm';
+import { MemberCtrl } from './member';
 
 interface IGetAllStats {
   birthdays: Member[];
   negativeBalanceUsers: Member[];
+  totalMembers: number;
 }
 
 export class DashboardCtrl {
@@ -20,7 +22,9 @@ export class DashboardCtrl {
       .leftJoinAndSelect('member.user', 'user')
       .leftJoinAndSelect('member.organisation', 'organisation')
       .where('organisation.id = :id', { id: orgID })
-      .andWhere('DATE_PART(\'doy\', user.birthdate) >= DATE_PART(\'doy\', CURRENT_DATE)')
+      .andWhere(
+        "DATE_PART('doy', user.birthdate) >= DATE_PART('doy', CURRENT_DATE)"
+      )
       .andWhere(
         `DATE_PART(\'doy\', user.birthdate) <= DATE_PART('doy', CURRENT_DATE + INTERVAL '${NBR_MIN_DAY_TO_INCLUDE_MEMBER} day')`
       )
@@ -41,20 +45,36 @@ export class DashboardCtrl {
       .getMany();
   }
 
+  private async getAllMembers(orgID: number): Promise<Member[]> {
+    const NBR_MIN_DAY_TO_INCLUDE_MEMBER = 7;
+    const memberRepo = getRepository(Member);
+
+    return memberRepo
+      .createQueryBuilder('member')
+      .leftJoinAndSelect('member.user', 'user')
+      .leftJoinAndSelect('member.organisation', 'organisation')
+      .where('organisation.id = :id', { id: orgID })
+      .getMany();
+  }
+
   public getAll = async (req: Request, res: Response): Promise<Response> => {
     const allStats: IGetAllStats = {
       birthdays: [],
-      negativeBalanceUsers: []
+      negativeBalanceUsers: [],
+      totalMembers: 0
     };
 
     const userRepo = getRepository(User);
+    const memberRepo = getRepository(Member);
     const currentUser = await userRepo.findOne(req.user.user.id);
-    const currentOrg = await currentUser.getUserOrganisation();
-
+    const currentOrg = await currentUser.getUserOrganisation();    
     if (!currentOrg) {
       return res.send(allStats);
     }
 
+    const members = await this.getAllMembers(currentOrg.id);
+    allStats.totalMembers = members.length;
+    
     allStats.birthdays = await this.getAllUpcomingMemberBirthdayInOrg(
       currentOrg.id
     );
