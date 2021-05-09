423a617e59ab6e9d187fa751d67fcf15a0100d4c
Arthur Verdon <arthur.verdon@liip.ch>
2021-05-09 22:59:21 +0200

    95 fixing bug on frontend picture

--- packages/frontend/src/components/molecules/Thumb.tsx
+++ packages/frontend/src/components/molecules/Thumb.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Image } from 'react-bootstrap';
 
 interface IProps {
@@ -10,32 +10,35 @@ interface IProps {
 export const Thumb = (props: IProps) => {
   let { src, size, alt } = props;
 
-  if(!size) {
+  if (!size) {
     size = 256;
   }
 
   const [isLoading, setIsLoading] = useState(true);
   const [thumb, setThumb] = useState<string>('');
 
+  useEffect(() => {
+    // Transform a file in a data URL
+    if (src instanceof File) {
+      let reader = new FileReader();
+      setIsLoading(true);
+
+      reader.onloadend = () => {
+        setIsLoading(false);
+        if (reader.result) {
+          setThumb(reader.result as string);
+        }
+      };
+
+      reader.readAsDataURL(src);
+    } else if (src !== thumb) {
+      setIsLoading(false);
+      setThumb(src as string);
+    }
+  }, [src, thumb]);
+
   if (!src) {
     return null;
-  }
-
-  // Transform a file in a data URL 
-  if (isLoading && src instanceof File) {
-    let reader = new FileReader();
-
-    reader.onloadend = () => {
-      setIsLoading(false);
-      if (reader.result) {
-        setThumb(reader.result as string);
-      }
-    };
-
-    reader.readAsDataURL(src);
-  } else if (isLoading) {
-    setIsLoading(false);
-    setThumb(src as string);
   }
 
   if (isLoading) {
