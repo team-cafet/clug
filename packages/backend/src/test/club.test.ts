920657f9cc15cb73d8da16b00c237422f8e6345b
Arthur Verdon <arthur.verdon@liip.ch>
2021-06-06 01:07:13 +0200

    138 Removing useless comments

--- packages/backend/src/test/club.test.ts
+++ packages/backend/src/test/club.test.ts
@@ -17,48 +17,8 @@ describe('Functionnal Club endpoint testing', () => {
     factory.app
       .get(API_ENDPOINT)
       .send()
-      .auth(factory.staffToken, {type: 'bearer'})
+      .auth(factory.staffToken, { type: 'bearer' })
       .set('Accept', 'application/json')
       .expect(200, done);
   });
-
-  //   describe('Basic Testing with admin', () => {
-
-  //     it('GET ALL', async (done) => {
-  //       expect(true);
-  //       // request(app)
-  //       //   .get(API_ENDPOINT)
-  //       //   .auth(adminUser.token, { type: 'bearer' })
-  //       //   .expect(200, done);
-  //     });
-
-  //     // it('POST', async (done) => {
-  //     //   request(app)
-  //     //     .post(API_ENDPOINT)
-  //     //     .auth(adminUser.token, { type: 'bearer' })
-  //     //     .send({
-  //     //       organisation: { id: 1 },
-  //     //       user: { email: 'test@test.ch', password: '1234' }
-  //     //     })
-  //     //     .expect(200, done);
-  //     // });
-
-  //     // it('GET ONE', async (done) => {
-  //     //   request(app)
-  //     //     .get(`${API_ENDPOINT}/1`)
-  //     //     .auth(adminUser.token, { type: 'bearer' })
-  //     //     .expect(200, done);
-  //     // });
-
-  //     // it('PUT', async (done) => {
-  //     //   request(app)
-  //     //     .put(`${API_ENDPOINT}/1`)
-  //     //     .auth(adminUser.token, { type: 'bearer' })
-  //     //     .send({
-  //     //       user: { email: 'test-updated@test.ch', password: '12346' }
-  //     //     })
-  //     //     .expect(200, done);
-  //     // });
-
-  //   });
 });
