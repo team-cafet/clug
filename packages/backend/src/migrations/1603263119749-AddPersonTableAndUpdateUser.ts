d30cf5df64d1b7f45efdfdc6f72e6d8e6f3fcac8
Sebibigbob8 <sebastien.aubert@hotmail.ch>
2022-05-01 12:28:56 +0200

    add several fields to person

--- packages/backend/src/migrations/1603263119749-AddPersonTableAndUpdateUser.ts
+++ packages/backend/src/migrations/1603263119749-AddPersonTableAndUpdateUser.ts
@@ -10,7 +10,7 @@ implements MigrationInterface
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      'CREATE TABLE "person" ("id" SERIAL NOT NULL, "email" character varying(254), "firstname" character varying(254), "lastname" character varying(254), "phone" character varying(50), PRIMARY KEY ("id"))'
+      'CREATE TABLE "person" ("id" SERIAL NOT NULL, PRIMARY KEY ("id"), "email" character varying(254), "firstname" character varying(254), "lastname" character varying(254), "phone" character varying(50), "sexe" "user_sexe_enum" DEFAULT \'0\',  "birthdate" date, "pictureURL" character varying, "street" character varying, "streetNumber" integer, "city" character varying, "postalCode" integer, "country" character varying)'
     );
     await queryRunner.addColumn(
       'user',
@@ -29,13 +29,13 @@ implements MigrationInterface
       })
     );
     await queryRunner.query(
-      'ALTER TABLE "user" DROP email, DROP firstname, DROP lastname, DROP phone'
+      'ALTER TABLE "user" DROP email, DROP firstname, DROP lastname, DROP "sexe", DROP "phone", DROP "birthdate", DROP "pictureURL", DROP "street", DROP "streetNumber", DROP "city", DROP "postalCode", DROP "country"'
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      'ALTER TABLE "user" ADD "email" character varying(254), ADD "firstname" character varying(254), ADD "lastname" character varying(254), ADD "phone" character varying(50)'
+      'ALTER TABLE "user" ADD "email" character varying(254), ADD "firstname" character varying(254), ADD "lastname" character varying(254) ,ADD "phone" character varying(50) ,ADD "sexe" "user_sexe_enum" DEFAULT \'0\', ADD "birthdate" date,ADD "pictureURL" character varying,ADD "street" character varying,ADD "streetNumber" integer,ADD "city" character varying, "postalCode" integer, ADD "country" character varying'
     );
     const userTable = await queryRunner.getTable('user');
     const foreignKey = userTable.foreignKeys.find(
