e48f924a882837e0d2649977b76eb80e8996ed9c
Arthur Verdon <arthur.verdon@liip.ch>
2023-03-05 11:19:08 +0100

    fix: adding S3 region

--- packages/backend/src/config/file.ts
+++ packages/backend/src/config/file.ts
@@ -19,7 +19,7 @@ export const fileConfig = (): IFileConfig => {
       secretAccessKey: process.env.S3_SECRET_KEY ?? 'test',
     },
     tls: process.env.S3_SSL === 'true' ?? false,
-    region: 'eu-west-1', // see https://qubyte.codes/blog/tip-connecting-to-localstack-s3-using-the-javascript-aws-sdk-v3
+    region: process.env.S3_REGION ?? 'eu-west-1', // see https://qubyte.codes/blog/tip-connecting-to-localstack-s3-using-the-javascript-aws-sdk-v3
     forcePathStyle: process.env.S3_FORCE_PATH_STYLE ? process.env.S3_FORCE_PATH_STYLE === 'true' : true,
     disableHostPrefix: true
   };
